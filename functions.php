b06306e6d6fe76efcde1c7b3ea37186bf45b835b
corvidism <a.frankova.contact@gmail.com>
2015-04-26 18:34:27 +0200

    moved form_date() function to functions.php (I need it in single.php too)

--- functions.php
+++ functions.php
@@ -27,5 +27,24 @@ function logme($message) {
 function logmeline($message) {
 	
 }
+			
+function form_date($date) {
+	//stupid formatting shiv. should be done somewhere else, not sure where tho. On db load?
+	
+	$date_array = explode("/", $date);
+	
+	//see if the format is "*/00/00" - if so, strip the last part, it's just a year.
+	//if not, turn this into a proper PHP date, then use PHP formatting to spit it out properly
+	
+	$year = $date_array[0];
+	if (isset($year[0]) && $year[0] == "-") {
+		return substr($year,1,strlen($year))."BCE";
+	} else {
+		return $year."CE";
+	}
+	
+	//don't echo empty year - if the second date is empty, echo only the first one
+}
+
 
 ?>
